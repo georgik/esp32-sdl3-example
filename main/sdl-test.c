a5d8e96d4336dcdcef2aa99f210faeeea863873b
Juraj Michálek <juraj.michalek@espressif.com>
2024-08-21 13:07:58 +0200

    initial attempt to call SDL_init

--- main/sdl-test.c
+++ main/sdl-test.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+#include "SDL3/SDL.h"
 void app_main(void)
 {
-    printf("Hello SDL3.");
+    printf("SDL3x");
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("Unable to initialize SDL");
+    } else {
+        printf("Initialized");
+    }
 }
