61c446aaeed1d6238316f90ad47ae88dbede0dd1
Juraj Michalek <juraj.michalek@gmail.com>
2024-10-02 18:55:50 +0200

    remove component files

--- components/georgik__sdl/src/video/esp-idf/SDL_espidfevents.h
+++
@@ -1,8 +0,0 @@
-#ifndef SDL_espidfevents_h_
-#define SDL_espidfevents_h_
-
-#include "SDL_internal.h"
-
-void ESPIDF_PumpEvents(SDL_VideoDevice *_this);
-
-#endif /* SDL_espidfevents_h_ */
--- components/georgik__sdl/src/video/esp-idf/SDL_espidfvideo.h
+++
@@ -1,8 +0,0 @@
-
-
-#ifndef SDL_espidfvideo_h_
-#define SDL_espidfvideo_h_
-
-#include "../SDL_sysvideo.h"
-
-#endif /* SDL_espidfvideo_h_ */
